75e6892561571b66743813b6d6232073571bc2b6
Hannah <hannahkowood@gmail.com>
2016-08-11 20:57:25 -0400

    cleansed

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,7 @@
 var router = require('express').Router();
 module.exports = router;
 
-router.use('/members', require('./members'));
+// router.use('/members', require('./members'));
 
 // Make sure this is after all of
 // the registered routes!

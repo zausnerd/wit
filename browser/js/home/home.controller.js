2b7d30c76dfe48084104151b4075f173b25f1362
David Zausner <zausnerd@gmail.com>
2016-08-12 17:56:04 -0400

    span

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,7 +1,11 @@
 app.controller('SocketCtrl', function ($log, $scope, chatSocket, messageFormatter, nickName) {
   $scope.nickName = nickName;
+  $scope.messageQueue = [];
   $scope.messageLog = 'Ready to chat!\n';
+  $scope.encryptFunction = '';
+  $scope.decryptFunction = '';
   $scope.sendMessage = function() {
+    console.log("HELLO");
     var match = $scope.message.match('^\/nick (.*)');
 
     if (angular.isDefined(match) && angular.isArray(match) && match.length === 2) {
@@ -9,13 +13,14 @@ app.controller('SocketCtrl', function ($log, $scope, chatSocket, messageFormatte
       var oldNick = nickName;
       nickName = match[1];
       $scope.message = '';
-      $scope.messageLog = messageFormatter(new Date(), 
-                      nickName, 'nickname changed - from ' + 
+      $scope.messageLog = messageFormatter(new Date(),
+                      nickName, 'nickname changed - from ' +
                         oldNick + ' to ' + nickName + '!') + $scope.messageLog;
       $scope.nickName = nickName;
     }
 
     $log.debug('sending message', $scope.message);
+    $scope.messageQueue.push($scope.message);
     chatSocket.emit('message', nickName, $scope.message);
     $scope.message = '';
   };
@@ -25,7 +30,7 @@ app.controller('SocketCtrl', function ($log, $scope, chatSocket, messageFormatte
     if (!data.payload) {
       $log.error('invalid message', 'event', event, 'data', JSON.stringify(data));
       return;
-    } 
+    }
     $scope.$apply(function() {
       $scope.messageLog = $scope.messageLog + messageFormatter(new Date(), data.source, data.payload);
     });
